63a3c4ce705749a019e0bca8badf9539fd5e2dca
Luiz Medina <luizmedina87@gmail.com>
2022-04-05 14:27:32 -0400

    added btns

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,8 +46,11 @@ class Artist {
     }
   }
   createHtmlElements() {
+    var matchesEl = document.getElementById("matches");
+    // deleting existing cards, if any
+    matchesEl.innerHTML = "";
+    // creating a card for each artist
     for (let i = 0; i < this.similar.length; i++) {
-      // debugger;
       var artist = this.similar[i];
       // creating element
       var cardNameEl = document.createElement("p");
@@ -55,29 +58,28 @@ class Artist {
       var similarityEl = document.createElement("p");
       similarityEl.textContent = artist.similarityIndex;
       var cardDivEl= document.createElement("div");
-      cardDivEl
-        .setAttribute("id", `${artist.name}-div`)
-        .appendChild(cardNameEl)
-        .appendChild(similarityEl);
+      cardDivEl.setAttribute("id", `${artist.name}-div`);
+      cardDivEl.appendChild(cardNameEl);
+      cardDivEl.appendChild(similarityEl);
       var capsuleCardEl = document.createElement("a");
-      capsuleCardEl
-        .setAttribute("href", `${artist.lastFmUrl}`)
-        .setAttribute("target", "_blank")
-        .appendChild(cardDivEl);
+      capsuleCardEl.setAttribute("href", `${artist.lastFmUrl}`);
+      capsuleCardEl.setAttribute("target", "_blank");
+      capsuleCardEl.appendChild(cardDivEl);
       // appending element
-      var matchesEl = document.getElementById("matches");
       matchesEl.appendChild(capsuleCardEl);
-      console.log(i);
+      // console.log(i);
     }
   }
 }
 
-async function searchArtist(artistStr, explode=true) {
+// CREATE TRY CATCH
+async function searchArtist(artistStr, explode=true, storeArtist=false) {
   // creating the url of the apis to get the artist data
   var apiAudioDb = (
     `https://www.theaudiodb.com/api/v1/json/2/search.php`
     + `?s=${artistStr}`
   );
+  // https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=travis&api_key=53346310a0cd2a63988f37c9d6e8a02e&limit=6&format=json
   var apiLastFm = (
     `https://ws.audioscrobbler.com/2.0/`
     + `?method=artist.getsimilar`
@@ -101,6 +103,7 @@ async function searchArtist(artistStr, explode=true) {
   ])
   // using API data to create object
   var similarArtistsData = lastFmData.similarartists.artist;
+  // debugger;
   var searchedArtistData = audioDbData.artists[0];
   if (explode) {
     var similarArtistObjs = [...Array(similarArtistsData.length)];
@@ -109,10 +112,17 @@ async function searchArtist(artistStr, explode=true) {
       similarArtistObjs[i].similarityIndex = similarArtistsData[i].match;
       similarArtistObjs[i].lastFmUrl = similarArtistsData[i].url;
     }
-    // console.log(similarArtistObjs);
     var searchedArtist = new Artist(searchedArtistData, similarArtistObjs);
-    // searchedArtist.createHtmlElements(); // FIX HERE
-    pushArtist(searchedArtist.name);
+    // creating cards
+    searchedArtist.createHtmlElements();
+    // clearing the form
+    document.getElementById(`search-artist`).value = "";
+    // saving artist to memory
+    if (storeArtist) {
+      pushArtist(searchedArtist.name);
+    }
+    // creating back button
+    makeBackBtn();
   }
   else {
     var similarArtist = new Artist(searchedArtistData);
@@ -135,9 +145,8 @@ function pushArtist(artistName) {
 function popArtist() {
   var artistsPile = localStorage.getItem("artistsPile");
   artistsPile = JSON.parse(artistsPile);
-  console.log("here");
   if (!artistsPile) {
-    console.log("no artists to pop")
+    console.log("no artists to pop");
   }
   else {
     var poppedArtist = artistsPile.pop();
@@ -146,14 +155,85 @@ function popArtist() {
   }
 }
 
+function readLastArtist() {
+  var artistsPile = localStorage.getItem("artistsPile");
+  artistsPile = JSON.parse(artistsPile);
+  if (!artistsPile) {
+    console.log("no artists to find");
+  }
+  else {
+    var poppedArtist = artistsPile.pop();
+    return poppedArtist;
+  }
+}
+
+function makeContinueBtn() {
+  var artistsPile = localStorage.getItem("artistsPile");
+  artistsPile = JSON.parse(artistsPile);
+  if (artistsPile) {
+    console.log("here");
+    var formHolderEl = document.getElementById("form-holder");
+    var pBtnEl = document.createElement("p");
+    pBtnEl.innerHTML = `<a href="#" id="continue-btn" class="pure-button pure-button-primary">Continue where you left off</a>`;
+    formHolderEl.appendChild(pBtnEl);
+    // lets the user continue where he left off
+    document.getElementById(`continue-btn`).addEventListener("click", function(event) {
+      event.preventDefault();
+      searchArtist(readLastArtist());
+      // hides button after clicking it
+      this.style.display = "none";
+    });
+  }
+}
+
+function makeBackBtn() {
+  // checking if there should be a back button
+  var artistsPile = localStorage.getItem("artistsPile");
+  artistsPile = JSON.parse(artistsPile);
+  if (artistsPile.length > 1) {
+    // clearing old button
+    try {
+      console.log("i am trying to create a back button");
+      document.getElementById("back-btn").remove();
+    }
+    catch {
+      console.log("no back button to remove");
+    }
+    // gettng the index of the next to last artist
+    var backIndex = artistsPile.length - 2;
+    var backArtist = artistsPile[backIndex];
+    // creating the button
+    var backBtnText = `Back to ${backArtist}`;
+    var backBtnEl = document.createElement("p");
+    var formHolderEl = document.getElementById("form-holder");
+    backBtnEl.innerHTML = `<a href="#" id="back-btn" class="pure-button pure-button-primary">${backBtnText}</a>`;
+    formHolderEl.appendChild(backBtnEl);
+    // make it clickable
+    backBtnEl.addEventListener("click", function() {
+      // removes the most recent artist searched
+      popArtist();
+      // gets the previous one and searches it again
+      searchArtist(popArtist(), true, true);
+    });
+  }
+}
+
+// Creates the continue button if there is something saved to local storage
+makeContinueBtn();
+
+// lets the user search for a new artist
 document.getElementById(`search-btn`).addEventListener("click", function(event) {
   event.preventDefault();
+  // clears the search history each time the user inputs a new artist
+  localStorage.setItem("artistsPile", "[]");
+  // hides continue button if the user didn't click it
+  try {
+    document.getElementById(`continue-btn`).style.display = "none";
+  }
+  catch {
+    console.log("no continue button");
+  }
+  // searches the new artist
   var userInput = document.getElementById(`search-artist`).value;
-  searchArtist(userInput);
+  searchArtist(userInput, true, true);
 });
-
-// BTN SHOULD ONLY SHOW IF LOCAL STORAGE EXISTS
-document.getElementById(`continue-btn`).addEventListener("click", function(event) {
-  event.preventDefault();
-  console.log(popArtist());
-})
